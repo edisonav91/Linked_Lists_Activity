8b826a1854642460364fa01fd2ab70511f4ad9b3
Edison Alvarez <edisonav1991@gmail.com>
2025-05-19 21:48:14 -0500

    Primera parte

---
+++ LinkedListsActivity/DoubleList.UI.Console/Program.cs
@@ -0,0 +1,71 @@
+﻿using DoubleList;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var list = new DoublyLinkedList<string>();
+        var opc = "0";
+
+        do
+        {
+            opc = Menu();
+            switch (opc)
+            {
+                case "1":
+                    Console.Write("Enter the data to insert at the beginning: ");
+                    var dataAtBeginning = Console.ReadLine();
+                    if (dataAtBeginning != null)
+                    {
+                        list.InsertAtBeginning(dataAtBeginning);
+                    }
+                    break;
+
+                case "2":
+                    Console.Write("Enter the data to insert at the end: ");
+                    var dataAtEnd = Console.ReadLine();
+                    if (dataAtEnd != null)
+                    {
+                        list.InsertAtEnd(dataAtEnd);
+                    }
+                    break;
+
+                case "3":
+                    Console.WriteLine(list.GetForward());
+                    break;
+
+                case "4":
+                    Console.WriteLine(list.GetBack.ward());
+                    break;
+
+                case "5":
+                    Console.Write("Enter the data to remove: ");
+                    var remove = Console.ReadLine();
+                    if (remove != null)
+                    {
+                        list.Remove(remove);
+                        Console.WriteLine("Item removed.");
+                    }
+                    break;
+            }
+        }
+        while (opc != "0");
+
+        string Menu()
+        {
+            Console.WriteLine("1. Adicionar.");
+            Console.WriteLine("2. Mostrar hacia adelante.");
+            Console.WriteLine("3. Mostrar hacia atrás.");
+            Console.WriteLine("4. Ordenar decentemente.");
+            Console.WriteLine("5. Mostrar la(s) moda(s).");
+            Console.WriteLine("6. Mostrar gráfico.");
+            Console.WriteLine("7. Existe.");
+            Console.WriteLine("8. Eliminar una ocurrencia.");
+            Console.WriteLine("9. Eliminar todas las ocurrencias.");
+            Console.WriteLine("0. Salir.");
+            Console.Write("Seleccione una opción: ");
+            return Console.ReadLine() ?? "0";
+        }
+
+    }
+}
---
+++ LinkedListsActivity/DubleList/DoublyLinkedList.cs
@@ -0,0 +1,107 @@
+﻿using DubleList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleList;
+
+public class DoublyLinkedList<T>
+{
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public DoublyLinkedList()
+    {
+        _tail = null;
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null) // Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null) // Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+    }
+
+    public string GetForward()
+    {
+        var output = string.Empty;
+        var current = _head;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Next;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    public string GetBackward()
+    {
+        var output = string.Empty;
+        var current = _tail;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Prev;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    public void Remove(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next; // Remove head
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev; // Remove tail
+                }
+
+                break;
+            }
+            current = current.Next;
+        }
+    }
+}
